66e5e46f9779184b3933131b894995912620ba73
aqeelraz146@gmail.com <aqeelrazagondal@gmail.com>
2018-05-25 14:28:52 +0500

    update register rpite

--- routes/users.js
+++ routes/users.js
@@ -57,14 +57,14 @@ router.post('/register', adminAuth, async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered.');
 
-  user = new User(_.pick(req.body, ['name', 'email', 'password', 'user_type']));
+  user = new User(_.pick(req.body, ['name', 'email', 'password', 'user_type', 'phone']));
 
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
   const token = user.generateAuthToken();
-  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email', 'user_type']));
+  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email', 'user_type', 'phone']));
 
   let user_type = req.body.user_type;
   if(user_type === 'driver'){
