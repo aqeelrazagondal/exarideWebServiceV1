52da83d56b7f5d532e67c12f2cb95440043dc2fb
khansaifullah <khansaifullah1993@gmail.com>
2018-07-08 00:44:25 +0500

    newOverSpeedAlert 3

--- models/overSpeedAlert.js
+++ models/overSpeedAlert.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
-var User = require('./user');
 var Driver = require('./driver');
 
 
 // Define our schema
-var OverSpeedAlertSchema   = new mongoose.Schema({
-    
+
+var overSpeedAlertSchema  = mongoose.model('OverSpeedAlert', new mongoose.Schema({
     _driverId: { type: mongoose.Schema.Types.ObjectId, ref: 'Driver'},
    msg:String,
    speed: Number    
-}, {timestamps: true});
+} , {timestamps: true}));
+
 // Export the Mongoose model
-module.exports = mongoose.model('OverSpeedAlert', OverSpeedAlertSchema);
+exports.OverSpeedAlert = overSpeedAlertSchema; 
