805a1d9cd75a1ef68f1c357fe777bcff188dbe35
khansaifullah <khansaifullah1993@gmail.com>
2018-07-09 11:07:40 +0500

    uPDATED sHARED mODEL

--- models/shared.js
+++ models/shared.js
@@ -2,9 +2,10 @@
 const mongoose = require('mongoose');
 
 // Define our schema
-const SharedSchema   = new mongoose.Schema({
+const sharedSchema   = new mongoose.Schema({
     speedLimit: { type: Number, default: 0}
 }, {timestamps: true});
 
 // Export the Mongoose model
-module.exports = mongoose.model('Shared', SharedSchema);
+const Shared = mongoose.model('Shared', sharedSchema);
+module.exports.Shared = Shared; 
