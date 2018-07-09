95e8e452dca8958cb311b9f20a338127784e9d75
khansaifullah <khansaifullah1993@gmail.com>
2018-07-09 18:16:33 +0500

    OverSpeedAlert to home

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+const { OverSpeedAlert } = require('../models/overSpeedAlert');
+
 router.get('/', (req, res) => {
     res.send('BMS - Smart Ride WebSerivices Working');
 });
+
+
+router.get('/overSpeedingAlert', async (req, res) => {
+    // const query = req.params.id;
+    const overSpeedAlerts = await OverSpeedAlert.find({  });
+    if (!overSpeedAlerts) return res.status(404).send('OverSpeedAlerts List is Empty.');
+    
+    res.jsonp({ status: 'Success', message: 'Over Speed Alerts List. ', object: overSpeedAlerts });
+  });
+  
 
 module.exports = router; 
